872d295a4f681b2b0c153627cd9b337f0e069bbf
jpablopachar <jpablopachar1993@gmail.com>
2024-11-27 17:06:38 -0500

    Se agrega funciones de UserController

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,32 @@
 import { Request, Response } from 'express'
+import { v4 as uuid } from 'uuid'
+
+interface CustomRequest extends Request {
+  user: IUser
+}
+
+import { UploadApiResponse } from 'cloudinary'
+import { Result, ValidationError, validationResult } from 'express-validator'
+import formidable from 'formidable'
+import IncomingForm from 'formidable/Formidable'
 import slug from 'slug'
-import User from '../models/user'
-import { hashPassword, responseReturn } from '../utils'
+import cloudinary from '../config/cloudinary'
+import User, { IUser } from '../models/user'
+import {
+  checkPassword,
+  generateJwt,
+  hashPassword,
+  responseReturn,
+} from '../utils'
 
 export const createAccount = async (req: Request, res: Response) => {
   try {
     const { email, password, handle: rawHandle } = req.body
+
+    if (!email || !password || !rawHandle)
+      return responseReturn(res, 400, {
+        error: 'Email, password and handle are required',
+      })
 
     if (await User.exists({ email }))
       return responseReturn(res, 400, { error: 'User already exists' })
@@ -15,16 +36,160 @@ export const createAccount = async (req: Request, res: Response) => {
     if (await User.exists({ handle }))
       return responseReturn(res, 409, { error: 'Handle already exists' })
 
-    const user = new User(req.body)
+    const hashedPassword: string = await hashPassword(password)
 
-    user.password = await hashPassword(password)
-    user.handle = handle
+    const user = new User({
+      ...req.body,
+      password: hashedPassword,
+      handle,
+    })
 
     await user.save()
 
     return responseReturn(res, 201, { message: 'User created' })
   } catch (error) {
-    console.log(`Error in createAccount: ${error}`)
+    console.error(`Error in createAccount: ${error}`)
+
+    return responseReturn(res, 500, { error: 'An unexpected error occurred' })
+  }
+}
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const errors: Result<ValidationError> = validationResult(req)
+
+    if (!errors.isEmpty())
+      return responseReturn(res, 400, { errors: errors.array() })
+
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    if (!user) return responseReturn(res, 404, { error: 'User not found' })
+
+    const isPasswordValid: boolean = await checkPassword(
+      password,
+      user.password
+    )
+
+    if (!isPasswordValid)
+      return responseReturn(res, 401, { error: 'Invalid password' })
+
+    const token = generateJwt({ id: user._id })
+
+    return responseReturn(res, 200, { token })
+  } catch (error) {
+    console.error(`Error in login: ${error}`)
+  }
+}
+
+export const getUser = async (req: CustomRequest, res: Response) => {
+  return responseReturn(res, 200, req.user)
+}
+
+export const updateProfile = async (req: CustomRequest, res: Response) => {
+  try {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty())
+      return responseReturn(res, 400, { errors: errors.array() })
+
+    const { description, links, handle: rawHandle } = req.body
+
+    const handle: string = slug(rawHandle, '')
+    const handleExists = await User.findOne({ handle })
+
+    if (handleExists && handleExists.email !== req.user.email) {
+      return responseReturn(res, 409, { error: 'Handle already exists' })
+    }
+
+    if (req.user) {
+      req.user.description = description
+      req.user.links = links
+      req.user.handle = handle
+
+      await req.user.save()
+
+      return responseReturn(res, 200, { message: 'Profile updated' })
+    } else {
+      return responseReturn(res, 404, { error: 'User not found' })
+    }
+  } catch (error) {
+    console.error(`Error in updateProfile: ${error}`)
+
+    return responseReturn(res, 500, { error: 'An unexpected error occurred' })
+  }
+}
+
+export const uploadImage = async (req: CustomRequest, res: Response) => {
+  const form: IncomingForm = formidable({ multiples: false })
+
+  try {
+    const { files } = await new Promise<{
+      fields: formidable.Fields
+      files: formidable.Files
+    }>((resolve, reject) => {
+      form.parse(req, (err, fields, files) => {
+        if (err) reject(err)
+        else resolve({ fields, files })
+      })
+    })
+
+    if (!files.file || !files.file[0])
+      return responseReturn(res, 400, { error: 'No file uploaded' })
+
+    const result: UploadApiResponse = await cloudinary.uploader.upload(
+      files.file[0].filepath,
+      { public_id: uuid() }
+    )
+
+    req.user.image = result.secure_url
+
+    await req.user.save()
+
+    return responseReturn(res, 200, { image: result.secure_url })
+  } catch (error) {
+    console.error(`Error in uploadImage: ${error}`)
+
+    return responseReturn(res, 500, { error: 'An unexpected error occurred' })
+  }
+}
+
+export const getUserByHandle = async (req: Request, res: Response) => {
+  try {
+    const { handle } = req.params
+
+    const user = await User.findOne({ handle }).select(
+      '-_id -__v -email -password'
+    )
+
+    if (!user) return responseReturn(res, 404, { error: 'User not found' })
+
+    return responseReturn(res, 200, user)
+  } catch (error) {
+    console.error(`Error in getUserByHandle: ${error}`)
+
+    return responseReturn(res, 500, { error: 'An unexpected error occurred' })
+  }
+}
+
+export const searchByHandle = async (req: Request, res: Response) => {
+  try {
+    const { handle } = req.body
+
+    if (!handle || typeof handle !== 'string')
+      return responseReturn(res, 400, {
+        error: 'Handle is required and string',
+      })
+
+    const user = await User.findOne({ handle })
+
+    if (user)
+      return responseReturn(res, 409, { error: `${handle} already exists` })
+
+    return responseReturn(res, 200, { message: `${handle} is available` })
+  } catch (error) {
+    console.error(`Error in searchByHandle: ${error}`)
 
     return responseReturn(res, 500, { error: 'An unexpected error occurred' })
   }
---
+++ src/middlewares/validation.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from 'express'
+import { Result, ValidationError, validationResult } from 'express-validator'
+
+export const handleInputErrors = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const errors: Result<ValidationError> = validationResult(req)
+
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
+
+  next()
+}
